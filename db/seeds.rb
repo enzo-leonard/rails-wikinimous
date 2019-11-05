2a8df1b2b0613ad1aca28ad582bc7f1440b3f0a3
enzo leonard <enzoleonard96@gmail.com>
2019-11-05 16:05:42 +0100

    "update"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do 
+   # p Faker::ChuckNorris.fact
+    Article.create(title: Faker::ChuckNorris.fact, content: Faker::Lorem.paragraph(sentence_count: 50) )
+end 
